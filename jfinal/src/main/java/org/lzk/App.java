6e3f265594ae0f2e2a38c4c79ba2d05d94574f87
18170 <18170603701@163.com>
2017-12-21 00:01:23 +0800

    first commit

---
+++ jfinal/src/main/java/org/lzk/App.java
@@ -0,0 +1,13 @@
+package org.lzk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
